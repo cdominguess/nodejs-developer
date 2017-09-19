d6d653865c9eb9d97eea04a70f4480f437cbbfea
Carlos F. Domingues <dominguess@gmail.com>
2017-09-19 00:10:50 -0300

    atualizando arquivos com as aulas de hoje

--- js-avancado/js/main.js
+++ js-avancado/js/main.js
@@ -4,21 +4,13 @@ var lista = [
 {"descricao": "produto 3", "qtde": "3", "valor": "15.4"}
 ];
 
-function getTotal(lista) {
-	var total = 0;
-	for (chave in lista) {
-		total += lista[chave].qtde * lista[chave].valor;
-	}
-
-	return total;
-}
-
 /**
  * Recebe um array e monta a lista de produtos em HTML
  * @param {array}
  */
  function setLista(lista) {
  	var linhas = '';
+ 	var total = 0;
 
  	for (chave in lista) {
  		linhas += '<tr>';
@@ -27,12 +19,16 @@ function getTotal(lista) {
  		linhas += '<td style="text-align: right;">'+formatValor(lista[chave].valor)+'</td>';
  		linhas += '<td style="text-align: center;">';
  		linhas += '<button class="btn-link" onclick="editarItem('+chave+');">Editar</button>|';
- 		linhas += '<button class="btn-link" onclick="editarItem('+chave+', "S");">Excluir</button>';
+ 		linhas += '<button class="btn-link" onclick="excluirItem('+chave+');">Excluir</button>';
  		linhas += '</td>';
  		linhas += '</tr>';
+
+ 		// Calcula o total dos itens
+ 		total += lista[chave].qtde * lista[chave].valor;
  	}
 
  	document.getElementById('list-table').innerHTML = linhas;
+ 	document.getElementById('valor-total').innerHTML = formatValor(total);
  }
 
 /**
@@ -40,15 +36,17 @@ function getTotal(lista) {
  * @return {void}
  */
  function adicionarItem() {
- 	var descricao = document.getElementById('descricao').value;	
- 	var qtde = document.getElementById('qtde').value;	
- 	var valor = document.getElementById('valor').value;
+ 	if (validarForm()) {
+ 		var descricao = document.getElementById('descricao').value;	
+ 		var qtde = document.getElementById('qtde').value;	
+ 		var valor = document.getElementById('valor').value;
 
-	// Usar unshift para adicionar o novo produto no início da lista
-	lista.unshift({'descricao': descricao, 'qtde': qtde, 'valor': valor});
+		// Usar unshift para adicionar o novo produto no início da lista
+		lista.unshift({'descricao': descricao, 'qtde': qtde, 'valor': valor});
 
-	// Por fim chama a função para renderizar a lista atualizada
-	setLista(lista);
+		// Por fim chama a função para renderizar a lista atualizada
+		setLista(lista);
+	}
 }
 
 /**
@@ -56,7 +54,7 @@ function getTotal(lista) {
  * @param  {integer} id
  * @return {void}
  */
-function editarItem(id) {
+ function editarItem(id) {
 	// Oculta o botão de adicionar e exibe os botões de atualizar
 	document.getElementById('btn-adicionar').style.display = 'none';
 	document.getElementById('btn-atualizar').style.display = 'inline';
@@ -73,33 +71,78 @@ function editarItem(id) {
  * Efetua a atualização de um produto
  * @return {void}
  */
-function atualizarItem() {
-	var id = document.getElementById('id-atualizar').value;
+ function atualizarItem() {
 
-	// Atualiza o produto na lista de objetos
-	lista[id].descricao = document.getElementById('descricao').value;
-	lista[id].qtde = document.getElementById('qtde').value;
-	lista[id].valor = document.getElementById('valor').value;
+ 	if (validarForm()) {
+ 		var id = document.getElementById('id-atualizar').value;
 
-	// Chama a função de exibir a lista de produtos, e reinicia o formulário
-	setLista(lista);
-	resetarForm();
+		// Atualiza o produto na lista de objetos
+		lista[id].descricao = document.getElementById('descricao').value;
+		lista[id].qtde = document.getElementById('qtde').value;
+		lista[id].valor = document.getElementById('valor').value;
+
+		// Chama a função de exibir a lista de produtos, e reinicia o formulário
+		setLista(lista);
+		resetarForm();
+	}
+}
+
+function excluirItem(id) {
+	if (confirm('Você tem certeza que deseja excluir este item?')) {
+		lista.splice(id, 1);
+		setLista(lista);
+	}
 }
 
 /**
  * Cancela a edição de um produto
  * @return {void}
  */
-function resetarForm() {
+ function resetarForm() {
 	// Oculta os botões de atualizar e exibe o botão de adicionar
 	document.getElementById('btn-adicionar').style.display = 'inline';
 	document.getElementById('btn-atualizar').style.display = 'none';
+	document.getElementById('erros').style.display = 'none';
 
 	// reseta o formulário
 	document.getElementById('descricao').value = null;
 	document.getElementById('qtde').value = null;
 	document.getElementById('valor').value = null;
 	document.getElementById('id-atualizar').value = null;
+}
+
+function validarForm() {
+	var descricao = document.getElementById('descricao').value;
+	var qtde = document.getElementById('qtde').value;
+	var valor = document.getElementById('valor').value;
+
+	var erros = '';
+
+	if (descricao === '') {
+		erros += '<p>Preencha o campo Descrição.</p>';
+	}
+
+	if (qtde === '') {
+		erros += '<p>Preencha o campo Quantidade.</p>';
+	} else if (qtde != parseInt(qtde)) {
+		erros += '<p>O campo Quantidade deve conter somente números.</p>';	
+	}
+
+	if (valor === '') {
+		erros += '<p>Preencha o campo Valor.</p>';
+	} else if (valor != parseFloat(valor)) {
+		erros += '<p>O campo Valor deve conter somente números.</p>';	
+	}
+
+	if (erros !== "") {
+		document.getElementById('erros').style.display = 'block';
+		document.getElementById('erros').innerHTML = '<h4>Corrija os erros antes de prosseguir:</h4>' + erros;
+
+		return false;
+	} else {
+		document.getElementById('erros').style.display = 'none';
+		return true;
+	}
 }
 
 /**
@@ -123,5 +166,7 @@ function resetarForm() {
  	return 'R$' + parseFloat(valor).toFixed(2).replace('.', ',') + '';
  }
 
- setLista(lista);
- console.log(getTotal(lista));
+/*
+ * Por fim chama a função que monta a lista de produtos na tela
+ */
+setLista(lista);
